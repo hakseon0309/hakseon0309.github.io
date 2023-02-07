154c3d2ebed1e352fe82e128d598580db74627bc
hakseon0309 <120485392+hakseon0309@users.noreply.github.com>
2023-02-08 01:29:08 +0900

    noname

--- js.js
+++ js.js
@@ -1,2 +1,5 @@
-const arr = [1,2,3];
-arr.forEach(console.log);
+$(".que").click(function() {
+    $(this).next(".anw").stop().slideToggle(300);
+   $(this).toggleClass('on').siblings().removeClass('on');
+   $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
+ });
