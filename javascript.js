1aefd277f35d980a0238fd5c159acd35966adfe1
hakseon0309 <120485392+hakseon0309@users.noreply.github.com>
2023-02-20 07:42:48 +0900

    noname

--- javascript.js
+++ javascript.js
@@ -1,14 +1,43 @@
-var curMode = 0;
+const promptDiv = document.querySelector('.prompt');
+const resultUl = document.querySelector('.result ul');
+const copyBtn = document.querySelector('#copy-btn');
+let selectedTags = [];
 
-function Light_Dark_Mode() {
-    if (curMode == 0) {
-        document.getElementById('wrapper').style.color = black;
-        document.getElementById('wrapper').style.backgroundColor = white;
-        curMode = 1;
+promptDiv.addEventListener('click', (event) => {
+  if (event.target.tagName === 'P') {
+    const selectedTag = event.target.innerText.trim();
+    const index = selectedTags.indexOf(selectedTag);
+
+    if (index === -1) {
+      if (selectedTags.length === 5) {
+        promptDiv.querySelectorAll('p').forEach((p) => {
+          if (selectedTags.indexOf(p.innerText.trim()) === -1) {
+            p.classList.remove('selected');
+          }
+        });
+        selectedTags = [];
+      }
+      selectedTags.push(selectedTag);
+      event.target.classList.add('selected');
+    } else {
+      selectedTags.splice(index, 1);
+      event.target.classList.remove('selected');
     }
-    else if (curMode == 1) {
-        document.getElementById('wrapper').style.color = white;
-        document.getElementById('wrapper').style.backgroundColor = black;
-    }
-    
-}
+  }
+});
+
+copyBtn.addEventListener('click', () => {
+  const tagsText = selectedTags.join(', ');
+  if (tagsText) {
+    navigator.clipboard.writeText(tagsText).then(() => {
+      alert(`${tagsText}가 클립보드에 복사되었습니다!`);
+    });
+  }
+  
+  selectedTags = [];
+  promptDiv.querySelectorAll('p').forEach((p) => {
+    p.classList.remove('selected');
+  });
+  
+  resultUl.innerHTML = '';
+});
