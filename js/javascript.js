cd29a5d81dbf941f3fe64136c31c72f2cbaf2cb7
hakseon0309 <120485392+hakseon0309@users.noreply.github.com>
2023-04-16 00:37:50 +0900

    230416

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    const skillDamage = 300; // 변경할 값을 변수로 지정합니다.
+    const skillDamageElement = document.getElementById('skill-damage'); // 값을 변경할 요소를 식별합니다.
+    skillDamageElement.innerText = `[${skillDamage}]`; // 요소 내의 값을 변수로 변경합니다.
+  }
+
+// 10.1 어둠의 도가니, 아베루스
+
+class AberrusBoss {
+    name = null
+    summary = null
+    Boss1() {
+        
+    } 
+}
+
